d1bde75760a14e995870f2820ab3c7b45c23cead
omatheusdutra <matheusbdutra@hotmail.com>
2023-03-03 19:17:08 -0300

    criando class Campeonato

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,8 +7,45 @@ public class PrimeiroPrograma {
         Moto moto = new Moto();
         System.out.println(moto);
 
-        /*        int a = 3;
+        Campeonato Campeonato = new Campeonato( "AMA Motocross", 12);
+        System.out.println(Campeonato);
+
+        /*int a = 3;
         int b = 5;
         System.out.println("Hello World!  " + (a+b));*/
+    }
+}
+
+class Campeonato {
+    private String nome;
+    private Integer numEtapas;
+
+    public Campeonato(String nome, Integer numEtapas) {
+        this.nome = nome;
+        this.numEtapas = numEtapas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumEtapas() {
+        return numEtapas;
+    }
+
+    public void setNumEtapas(Integer numEtapas) {
+        this.numEtapas = numEtapas;
+    }
+
+    @Override
+    public String toString() {
+        return "Campeonato{" +
+                "nome='" + nome + '\'' +
+                ", numEtapas=" + numEtapas +
+                '}';
     }
 }
